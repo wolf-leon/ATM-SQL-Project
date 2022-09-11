e80c22225e320d478ad1f4e640ac4c46e8022f10
michelle <mbarbosa@oneshield.com>
2022-09-11 13:34:17 +0530

    PIN Verification Test

--- ATM UI/pserver.php
+++ ATM UI/pserver.php
@@ -12,6 +12,7 @@ $tbl_name="card"; // Table name
 // Connect to server and select database.
 $db=mysqli_connect("$host", "$username", "$password","$db_name")or die("cannot connect");
 $errors=array();
+$c=0;
 
 if (isset($_POST['pbutton'])) {
   //data sanitization to prevent SQL injection
@@ -23,7 +24,6 @@ if (isset($_POST['pbutton'])) {
     array_push($errors, "PIN Required"); 
 
   }
-  $c=0;
   if (count($errors) == 0) {
   //checking for the errors
     $query = "SELECT * FROM $tbl_name WHERE pin='$pname' AND card_num='$_SESSION[fname]';";
@@ -33,12 +33,13 @@ if (isset($_POST['pbutton'])) {
     if (mysqli_num_rows($results) == 1 and $c<=3 ) {
       // Welcome message
       $_SESSION['success'] = "You have logged in!";
-      header('location: index.html'); //page on which the user is sent to after logging in
+      header('location: options.html'); //page on which the user is sent to after logging in
     }
     else {
-        $c++;
             if ($c<3){
                 array_push($errors, "Incorrect PIN. Try Again."); 
+                $c++;
+
                 
             }
             else{
