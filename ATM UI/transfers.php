c0766d7b08682581d1a4406b32e7371ddddd37ad
Nadia Fernandes <@gmail.com>
2022-09-13 01:00:04 +0530

    Added transfers.php, transfer_server.php, tranfer_receipt

--- ATM UI/transfers.php
+++ ATM UI/transfers.php
@@ -1,4 +1,4 @@
-<?php include('server.php') ?>
+<?php include('transfer_server.php') ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -27,39 +27,44 @@
     </head>
 
 
-    <body style="background:linear-gradient(#D5F7E6,#63DFA8); background-attachment:fixed;">
+    <body style="background:linear-gradient(#D5F7E6, #63DFA8); background-attachment:fixed;">
        <!-- transfer of funds page code here -->
        <a href="index.html" style="color: #000000;"><i class="material-icons" style="font-size:30px; float: left;">home</i></a>
        <a href="options.html" style="color: #000000;"><i class="material-icons" style="font-size:30px; float: left;">list</i></a>
        <a href="end.html" style="color: #000000;"><i class="material-icons" style="font-size:30px; float: left;">exit_to_app</i></a>
      
+
       <div class="x">
         <img src="images\bank logo.png" alt="bank logo" style="width:150px;height:100px;";>
       </div> 
 
       <div class="row mx-auto">
         <div class="col col-lg-6 text-center">
-      <img src="images\fund_transfer1.png" alt="fund_img" style="width:550px;height:550px;  margin-left: 50px;">
+      <img src="images\fund_transfer1.png" alt="fund_img" style="width:500px;height:500px;  margin-left: 50px;">
         </div>
 
         <div class="col col-lg-6 text-center mx-auto">
          <div class=" text-center" style="margin-top: 80px;"> 
             <div class="mx-auto text-center m" ></div>
     <div class="mx-auto text-center" >
-    <form action="transfer_receipt.html" method="post" >
-      <p style="font-weight: bold;font-size: 60px;" > Transfer Funds </p>
-     <div class="text-center" style="margin-bottom:20px;">
+    <form action="transfers.php" method="post" >
+        <?php include('errors.php') ?>
+
+    <p style="font-weight: bold;font-size: 60px;" > Transfer Funds </p>
+    <div class="text-center" style="margin-bottom:20px;">
+      
+     
       <label>Card Number:</label>
-       <input type="text" id="card_no" name="card_no" placeholder=" Enter Card Number" required><br>
+        <input type="text" id="card_no" name="card_no" placeholder=" Enter Card Number" required><br>
      </div>
 
      <div class="text-center" style="margin-bottom:50px;">
       <label>Amount:</label>
         <input type="text" id="amt" name="amount" placeholder=" Enter Amount "><br>
-     </div>
+      </div>
 
         <div class="text-center" style="margin-bottom: 20px;">
-            <input type="submit" value="Submit" class="rounded-pill btn btn-primary g">
+            <input type="submit" value="Submit" name="submit" class="rounded-pill btn btn-primary g">
         </div>
 
         <div class="text-center" style="margin-bottom: 20px;">
